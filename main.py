04af2c89a60935b9b9c90b81bf57b1e2460f9b75
extrasauceontop <msuchemba@gmail.com>
2023-08-03 20:01:28 -0500

    added circle CI

---
+++ main.py
@@ -0,0 +1,8 @@
+def add_stuff(a, b):
+    return a + b
+
+def say_hi():
+    print("saying hi")
+
+if __name__ == "__main__":
+    say_hi()
---
+++ main_test.py
@@ -0,0 +1,9 @@
+from main import add_stuff
+
+def test_add():
+    assert add_stuff(2, 3) == 5
+    print("worked")
+
+if __name__ == "__main__":
+    test_add()
+
